7d40f6a81f73331647ad741554e7f3249b0defa1
volosgoto <volosgoto@yahoo.com>
2018-08-14 10:27:20 +0300

    all done GET request

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 let http = new easyHTTP;
 
 // Get posts
-let url = 'https://jsonplaceholder.typicode.com/posts';
-let posts = http.get(url, function (posts) {
+let url = 'https://jsonplaceholder.typicode.com/posts1';
+http.get(url, function (err, posts) {
+    if (err) {
+        console.log(err);
+    } else {
     console.log(posts);
+    }
 }); 
 
 // console.log(posts);
