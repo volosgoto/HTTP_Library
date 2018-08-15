feba6094d61250e4acced5e9b263a38cf837b42c
volosgoto <volosgoto@yahoo.com>
2018-08-15 12:36:24 +0300

    upd PUT to promise

--- app2.js
+++ app2.js
@@ -13,13 +13,29 @@
 
 
 // Add user data
+// let data = {
+//     "name": "new Name",
+//     "username": "Bret",
+//     "email": "test@test.com",
+// }
+// // Create POST
+// let url = 'https://jsonplaceholder.typicode.com/users';
+// http.post(url, data)
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err));
+
+
+// Update post 
+// Add user data
 let data = {
-    "name": "new Name",
-    "username": "Bret",
-    "email": "test@test.com",
+    "name": "Name",
+    "username": "Bt",
+    "email": "test1@test.com",
 }
-// Create POST
-let url = 'https://jsonplaceholder.typicode.com/users';
-http.post(url, data)
+
+let userId = 1;
+let url = `https://jsonplaceholder.typicode.com/users/${userId}`;
+
+http.put(url, data)
     .then(data => console.log(data))
     .catch(err => console.log(err));
--- lib2.js
+++ lib2.js
@@ -30,7 +30,19 @@
      };
 
      // Make HTTP PUT request
-     update(){};
+     put(url, data){
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                method: 'PUT',
+                headers: { 'Content-type' : 'application/json' },
+                body: JSON.stringify(data)
+                }
+            )
+            .then(res => res.json())
+            .then(data => resolve(data))
+            .catch(err => reject(err))
+            });
+     };
 
      // Make HTTP DELETE request
      delete(){};
