43a5851f85fb428fd691761439364679994316ac
volosgoto <volosgoto@yahoo.com>
2018-08-15 11:35:03 +0300

    add get() method

--- app2.js
+++ app2.js
@@ -2,3 +2,7 @@
  * Library for HTTP Requests
  * @version 2.0
  */
+let url = 'https://jsonplaceholder.typicode.com/posts';
+ 
+let http = new HTTP();
+http.get(url);
--- lib2.js
+++ lib2.js
@@ -2,3 +2,22 @@
  * Library for HTTP Requests
  * @version 2.0
  */
+
+ class HTTP {
+     // Make HTTP GET request
+     get(url){
+         fetch(url)
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(err => console.log(err))
+     };
+
+     // Make HTTP POST request
+     post(){};
+
+     // Make HTTP PUT request
+     update(){};
+
+     // Make HTTP DELETE request
+     delete(){};
+ }
