bbe582da90cbe6e22cb4a409b754873aab1ca406
volosgoto <volosgoto@yahoo.com>
2018-08-15 12:31:10 +0300

    upd POST  to promise

--- app2.js
+++ app2.js
@@ -3,10 +3,23 @@
  * @version 2.0
  */
 
-let http = new HTTP();
+ let http = new HTTP();
 
-// let url = 'https://jsonplaceholder.typicode.com/posts';
+// GET users 
+// let url = 'https://jsonplaceholder.typicode.com/users';
+// http.get(url) // method GET returns promise
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err));
+
+
+// Add user data
+let data = {
+    "name": "new Name",
+    "username": "Bret",
+    "email": "test@test.com",
+}
+// Create POST
 let url = 'https://jsonplaceholder.typicode.com/users';
-http.get(url)
+http.post(url, data)
     .then(data => console.log(data))
-    .catch((err) => console.log(err));
+    .catch(err => console.log(err));
--- lib2.js
+++ lib2.js
@@ -15,7 +15,19 @@
      };
 
      // Make HTTP POST request
-     post(){};
+     post(url, data){
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                method: 'POST',
+                headers: { 'Content-type' : 'application/json' },
+                body: JSON.stringify(data)
+                }
+            )
+            .then(res => res.json())
+            .then(data => resolve(data))
+            .catch(err => reject(err))
+            });
+     };
 
      // Make HTTP PUT request
      update(){};
