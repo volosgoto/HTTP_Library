74082edaa5ce1536b9caab6e48bdd78d8376922f
volosgoto <volosgoto@yahoo.com>
2018-08-15 15:24:59 +0300

    upd GET to async await

--- app3.js
+++ app3.js
@@ -2,3 +2,49 @@
  * Library for HTTP Requests
  * @version 3.0
  */
+
+let http = new HTTP();
+
+// GET users 
+let url = 'https://jsonplaceholder.typicode.com/users';
+http.get(url) // method GET returns promise
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+
+
+// Add user data
+// let data = {
+//     "name": "new Name",
+//     "username": "Bret",
+//     "email": "test@test.com",
+// }
+// // Create POST
+// let url = 'https://jsonplaceholder.typicode.com/users';
+// http.post(url, data)
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err));
+
+
+// Update User
+// Add user data
+// let data = {
+//     "name": "Name",
+//     "username": "Bt",
+//     "email": "test1@test.com",
+// }
+
+// let userId = 1;
+// let url = `https://jsonplaceholder.typicode.com/users/${userId}`;
+
+// http.put(url, data)
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err));
+
+
+// Delete User
+// let userId = 1;
+// let url = `https://jsonplaceholder.typicode.com/users/${userId}`;
+
+// http.delete(url)
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err));
--- lib3.js
+++ lib3.js
@@ -2,3 +2,56 @@
  * Library for HTTP Requests
  * @version 3.0
  */
+
+class HTTP {
+    // Make HTTP GET request
+    async get(url){
+        const response = await fetch(url);
+        const responseData = response.json();
+        return responseData;
+    };
+
+    // Make HTTP POST request
+    post(url, data){
+       return new Promise((resolve, reject) => {
+           fetch(url, {
+               method: 'POST',
+               headers: { 'Content-type' : 'application/json' },
+               body: JSON.stringify(data)
+               }
+           )
+           .then(res => res.json())
+           .then(data => resolve(data))
+           .catch(err => reject(err))
+           });
+    };
+
+    // Make HTTP PUT request
+    put(url, data){
+       return new Promise((resolve, reject) => {
+           fetch(url, {
+               method: 'PUT',
+               headers: { 'Content-type' : 'application/json' },
+               body: JSON.stringify(data)
+               }
+           )
+           .then(res => res.json())
+           .then(data => resolve(data))
+           .catch(err => reject(err))
+           });
+    };
+
+    // Make HTTP DELETE request
+    delete(url){
+       return new Promise((resolve, reject) => {
+           fetch(url, {
+               method: 'DELETE',
+               headers: { 'Content-type' : 'application/json' },
+               }
+           )
+           .then(res => res.json())
+           .then(() => resolve('Resorse deleted'))
+           .catch(err => reject(err))
+           });
+    };
+}
